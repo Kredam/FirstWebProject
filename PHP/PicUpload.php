1c47c068d4aea962c8b15074c646de76536a631d
gzsoci <gubizsolti@gmail.com>
2021-04-14 19:59:10 +0200

    ok

--- PHP/PicUpload.php
+++ PHP/PicUpload.php
@@ -8,5 +8,9 @@
 if( $_FILES["profile-pic"]["type"] == "image/jpeg" or $_FILES["profile-pic"]["type"] == "image/png")
 {
     move_uploaded_file($_FILES["profile-pic"]["tmp_name"] , "../Pictures/".$_FILES["profile-pic"]["name"]);
+    echo "A képecske feltöltésre került!";
+}
+else{
+    echo "A fájl feltöltés nem sikerült!";
 }
 ?>
