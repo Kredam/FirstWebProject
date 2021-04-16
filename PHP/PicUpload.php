326d7a80217789ab3d6fc5121cb16f0f22d363cc
gzsoci <gubizsolti@gmail.com>
2021-04-16 14:28:49 +0200

    ok

--- PHP/PicUpload.php
+++ PHP/PicUpload.php
@@ -1,10 +1,5 @@
 <meta charset="utf-8">
 <?php
-//fájl típusú paraméter fogadása
-// $_FILES ["paraméter neve"]["tulajdonságok"]
-// $_FILES ["kep"]["name"] , $_FILES["kep"]["type"]
-//print( $_FILES["kep"]["name"]." , ".$_FILES["kep"]["type"] );
-
 if( $_FILES["profile-pic"]["type"] == "image/jpeg" or $_FILES["profile-pic"]["type"] == "image/png")
 {
     move_uploaded_file($_FILES["profile-pic"]["tmp_name"] , "../Pictures/".$_FILES["profile-pic"]["name"]);
